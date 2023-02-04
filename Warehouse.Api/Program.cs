4c65f5049896b85fad1d31986bb50773cb3a895c
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-02-04 17:32:54 +0100

    Added initial set of files for demo

---
+++ Warehouse.Api/Program.cs
@@ -0,0 +1,29 @@
+using Core.WebApi.Middlewares.ExceptionHandling;
+using Warehouse;
+using Warehouse.Api.Middlewares.ExceptionHandling;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddRouting()
+    .AddWarehouseServices()
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseExceptionHandlingMiddleware()
+    .UseRouting()
+    .UseEndpoints(endpoints =>
+    {
+        endpoints.UseWarehouseEndpoints();
+    })
+    .ConfigureWarehouse()
+    .UseSwagger()
+    .UseSwaggerUI();
+
+app.Run();
+
+public partial class Program
+{
+}
